8545f0b814219f3b500116fdec2d9837853281d8
nadtokarina <karina.nadtoka03@mail.ru>
2024-05-30 11:38:52 +0300

    lab 2 has done

---
+++ pages/Days.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+
+
+function Days() {
+    return (
+        <View style={styles.contauner}></View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
--- pages/Home.js
+++ pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Button, ScrollView} from 'react-native';
 
 export default function Home() {
     return (
         <View style={styles.container}>
+            <View style={styles.inputWrapper}>
+                <TextInput style={styles.textInput} placeholder='Найти город...' />
+                <View style={styles.buttonWrapper}><Button title='Найти' color={'#fff'} /></View>
+            </View>
             <Text style={styles.location}>Moscow, Russia</Text>
             <Text style={styles.day}>Tue, Jun 30</Text>
             <View style={styles.degreesWrapper}>
@@ -15,9 +19,64 @@ export default function Home() {
             </View>
             <View style={styles.weatherList}>
                 <TouchableOpacity style={styles.weatherItem}>
-                    <Text style={styles.weatherTitle}>RainFall</Text>
+                    <View style={styles.weatherItemWrapper}>
+                        <Image style={styles.whetherItemImg} source={require('../assets/img/rain-icon.png')} />
+                        <Text style={styles.weatherTitle}>RainFall</Text>
+                    </View>
+                    <Text style={styles.weatherMeasure}>3cm</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.weatherItem}>
+                    <View style={styles.weatherItemWrapper}>
+                        <Image style={styles.whetherItemImg} source={require('../assets/img/wind-icon.png')} />
+                        <Text style={styles.weatherTitle}>Wind</Text>
+                    </View>
+                    <Text style={styles.weatherMeasure}>19km/h</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.weatherItem}>
+                    <View style={styles.weatherItemWrapper}>
+                        <Image style={styles.whetherItemImg} source={require('../assets/img/humidity-icon.png')} />
+                        <Text style={styles.weatherTitle}>Humidity</Text>
+                    </View>
+                    <Text style={styles.weatherMeasure}>64%</Text>
                 </TouchableOpacity>
             </View>
+            <ScrollView style={styles.scrollDays} horizontal={true}>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>Now</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>19°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>13:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>21°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>14:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>20°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>15:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>20°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>16:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>20°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>17:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>15°</Text>
+                </View>
+                <View style={styles.dayTimeWrapper}>
+                    <Text style={styles.dayTime}>18:00</Text>
+                    <Image style={styles.weatherTimeImg} source={require('../assets/img/weatherTime1.png')}/>
+                    <Text style={styles.weatherNow}>13°</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -30,16 +89,20 @@ const styles=StyleSheet.create({
         backgroundColor: "#E2A272",
     },
     location: {
+        marginTop: 20,
         color: "#303345",
-        fontSize: 20,
-        fontWeight: 900,
+        fontSize: 25,
+        fontWeight: '700',
+        alignSelf: 'center',
     },
     day: {
         color: "#9A938C",
         fontSize: 16,
+        fontWeight: '500',
+        alignSelf: 'center',
     },
     degreesWrapper: {
-        paddingTop: 60,
+        paddingTop: 40,
         flexDirection: 'row', 
     },
     imgWeather: {
@@ -49,9 +112,96 @@ const styles=StyleSheet.create({
     degree: {
         fontSize: 60,
         fontWeight: 900,
+        color: '#303345',
     },
     weather: {
         fontSize: 30,
         fontWeight: 200,
+        color: '#303345',
     },
+    weatherItem: {
+        margin: 3,
+        width: '85%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        alignSelf: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.36)',
+        padding: 7,
+        borderRadius: 10,
+        borderColor: 'rgba(255, 255, 255, 0.3)',
+        borderWidth: '0.5',
+        shadowColor: 'black', 
+        shadowOffset: { width: 0, height: 0 }, 
+        shadowRadius: 2, 
+        shadowOpacity: 0.2,
+    },
+    weatherItemWrapper: {
+        flexDirection: 'row',
+    },
+    whetherItemImg: {
+        width: 55,
+        height: 55,
+        marginTop: 5
+    },
+    weatherTitle: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#303345',
+        marginTop: 20,
+    },
+    weatherMeasure: {
+        marginTop: 19,
+        marginRight: 15,
+        fontSize: 16,
+        color: '#303345',
+    },
+    inputWrapper: {
+        flexDirection: 'row',
+    },
+    textInput: {
+        margin: 10,
+        padding: 3,
+        marginTop: -10,
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        width: '75%',
+        height: 25,
+        borderRadius: 3,
+    },
+    buttonWrapper: {
+        backgroundColor: 'rgba(48, 51, 69, 0.5)',
+        borderRadius: 3,
+        height: 25,
+        marginTop: -10,
+        marginLeft: -5,
+    },
+    scrollDays: {
+        margin: 10,
+    },
+    dayTimeWrapper: {
+        margin: 5,
+        marginTop: 10,
+        width: 100,
+        height: 150,
+        backgroundColor: 'rgba(255, 255, 255, 0.36)',
+        borderRadius: 20,
+    },
+    dayTime: {
+        paddingTop: 15,
+        fontSize: 16,
+        color: '#303345',
+        alignSelf: 'center',
+    },
+    weatherTimeImg: {
+        width: 70,
+        height: 70,
+        marginTop: 5,
+        alignSelf: 'center',
+    },
+    weatherNow: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: '#303345',
+        alignSelf: 'center',
+    }
 })
